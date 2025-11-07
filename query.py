7f86303cf9541d3586fdb2fec96bcdd0213215b4
archiekinnane <40501353+archiekinnane@users.noreply.github.com>
2025-11-06 20:02:27 -0500

    feat: Inserted data. Modified query.py to match output.

--- query.py
+++ query.py
@@ -5,9 +5,43 @@ with sqlite3.connect('concrete.db') as conn:
     cursor = conn.cursor()
 
     # 1. SHOW ALL TESTS
+    print('ALL TESTS')
+
+    query1 = '''SELECT a.*
+                , CASE WHEN a.passed = 1 then "PASS" else "FAIL" end as "result_text" 
+                FROM concrete_tests a '''
+    cursor.execute(query1)
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(f"{row['project_name']}: {row['actual_strength']} PSI - {row['result_text']}")
+
 
 
     # 2. Show ONLY failed tests
+    print('\nFAILED TESTS')
+    query2 = 'SELECT * from concrete_tests where passed = 0';
+    cursor.execute(query2)
+    
+    while row := cursor.fetchone():
+        print(f"{row['project_name']} on {row['test_date']}\n  "
+             f"Required: {row['required_strength']} PSI\n  Actual: {row['actual_strength']} PSI\n")
+
+    #rows = cursor.fetchall()
+    #for row in rows:
+      #  print(f"{row['project_name']} on {row['test_date']}\n  "
+      #        f"Required: {row['required_strength']} PSI\n  Actual: {row['actual_strength']} PSI\n")
+
 
 
     # 3. Count tests by project
+    print('TESTS PER PRODUCT')
+    query3 = '''SELECT project_name
+            , SUM(CASE WHEN passed = 1 then 1 else 0 end) as passed_tests, count(*) as all_tests
+            from concrete_tests group by project_name'''
+    
+    cursor.execute(query3)
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(f"{row['project_name']}: {row['passed_tests']}/{row['all_tests']} passed")
